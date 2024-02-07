bb101f6c72fe744e89f518fa46074f691aaa314c
Vlado <v.vladinov@student.fontys.nl>
2024-02-07 18:13:31 +0200

    Changes to the db

--- sql/mydb_user_files_info.sql
+++ sql/mydb_user_files_info.sql
@@ -16,35 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `encrypted_files_info`
+-- Table structure for table `user_files_info`
 --
 
-DROP TABLE IF EXISTS `encrypted_files_info`;
+DROP TABLE IF EXISTS `user_files_info`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `encrypted_files_info` (
+CREATE TABLE `user_files_info` (
   `File_id` int NOT NULL,
-  `User_id` int DEFAULT NULL,
-  `File_name` varchar(255) DEFAULT NULL,
-  `File_data` varbinary(255) DEFAULT NULL,
-  `Encryption_key_id` int NOT NULL,
-  `Is_encrypted` tinyint(1) DEFAULT NULL,
-  `Upload_date` datetime DEFAULT NULL,
+  `File_name` varchar(255) NOT NULL,
+  `File_type` varchar(45) NOT NULL,
+  `File_size` varchar(45) NOT NULL,
+  `Upload_date` datetime NOT NULL,
   PRIMARY KEY (`File_id`),
-  UNIQUE KEY `Encryption_key_id_UNIQUE` (`Encryption_key_id`),
-  KEY `User_id` (`User_id`),
-  CONSTRAINT `encrypted_files_info_ibfk_1` FOREIGN KEY (`User_id`) REFERENCES `users` (`User_id`),
-  CONSTRAINT `encrypted_files_info_ibfk_2` FOREIGN KEY (`Encryption_key_id`) REFERENCES `encryption_keys` (`UserKey_en_id`)
+  CONSTRAINT `fk_user_files_info_users` FOREIGN KEY (`File_id`) REFERENCES `users` (`User_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `encrypted_files_info`
+-- Dumping data for table `user_files_info`
 --
 
-LOCK TABLES `encrypted_files_info` WRITE;
-/*!40000 ALTER TABLE `encrypted_files_info` DISABLE KEYS */;
-/*!40000 ALTER TABLE `encrypted_files_info` ENABLE KEYS */;
+LOCK TABLES `user_files_info` WRITE;
+/*!40000 ALTER TABLE `user_files_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_files_info` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -56,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-01-20 15:22:22
+-- Dump completed on 2024-02-07 18:11:09
--- sql/mydb_users_passwords.sql
+++ sql/mydb_users_passwords.sql
@@ -26,7 +26,8 @@ CREATE TABLE `users_passwords` (
   `User_id` int NOT NULL AUTO_INCREMENT,
   `pass_hash` varchar(64) NOT NULL,
   `deleted` tinyint NOT NULL,
-  PRIMARY KEY (`User_id`)
+  PRIMARY KEY (`User_id`),
+  CONSTRAINT `fk_users_passwords_users1` FOREIGN KEY (`User_id`) REFERENCES `users` (`User_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -49,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-01-20 15:22:22
+-- Dump completed on 2024-02-07 18:11:10
